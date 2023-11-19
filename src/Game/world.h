313941ab45b8972b62ca3e399f20f1c1c81d827f
Yanis <yanis.kernoua@epitech.eu>
2023-11-19 18:58:51 +0100

    feat(src): ✨ default world texture

--- src/Game/world.h
+++ src/Game/world.h
@@ -8,9 +8,13 @@
 #pragma once
 #include "Game/entity.h"
 #include <SFML/Graphics/Types.h>
+#define WORLD_BACKGROUND_PATH "src/assets/world/background.jpg"
 
 typedef struct world_s {
     sfTexture *background_texture;
     sfSprite *background_sprite;
     entity_t **entities;
 } world_t;
+
+int world_set_background(world_t *world, char *texture_path);
+world_t *create_world(void);
